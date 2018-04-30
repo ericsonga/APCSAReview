dbb156da815f571be40717939b36a8fe867d35d5
ericsonga <ericson@cc.gatech.edu>
2018-04-30 11:05:52 -0400

    Fix back to apcsareview to get it to build

--- pavement.py
+++ pavement.py
@@ -26,20 +26,20 @@ if master_url is None:
         master_url = 'http://127.0.0.1:8000'
 
 master_app = 'runestone'
-serving_dir = "./build/JavaReview"
+serving_dir = "./build/apcsareview"
 dest = '../../static'
-project_name = "javareview"
+project_name = "apcsareview"
 
 options(
     sphinx = Bunch(docroot=".",),
 
     build = Bunch(
-        builddir="./build/JavaReview",
+        builddir="./build/apcsareview",
         sourcedir="_sources",
-        outdir="./build/JavaReview",
+        outdir="./build/apcsareview",
         confdir=".",
-        project_name = "javareview",
-        template_args={'course_id': 'javareview',
+        project_name = "apcsareview",
+        template_args={'course_id': 'apcsareview',
                        'login_required':'false',
                        'appname':master_app,
                        'loglevel': 10,
@@ -47,7 +47,7 @@ options(
                        'use_services': 'true',
                        'python3': 'false',
                        'dburl': 'postgresql://runestone@localhost/runestone',
-                       'basecourse': 'javareview'
+                       'basecourse': 'apcsareview'
                         }
     )
 )
@@ -58,4 +58,3 @@ if 'DBHOST' in environ and  'DBPASS' in environ and 'DBUSER' in environ and 'DBN
     options.build.template_args['dburl'] = 'postgresql://{DBUSER}:{DBPASS}@{DBHOST}/{DBNAME}'.format(**environ)
 
 from runestone import build  # build is called implicitly by the paver driver.
-
